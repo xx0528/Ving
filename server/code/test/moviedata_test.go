395ac7ee2f08ecd6bc70f556c0c866dcbc9a37d0
xiaoxin <xiaoxin@gmail.com>
2020-01-08 20:58:53 +0800

    测试数据

--- server/code/test/moviedata_test.go
+++ server/code/test/moviedata_test.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"testing"
 	"os"
-	"server/i18n"
 	"server/model"
 	"server/database"
 	"time"
@@ -15,22 +14,18 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
-
-func TestInsertMovie(t *testing.T) {
+func TestInsertUsers(t *testing.T) {
 
 	var (
 		err           error
 		collection    *mongo.Collection
-		insertOneRes  *mongo.InsertOneResult
 		insertManyRes *mongo.InsertManyResult
-		usersArray    = GetUserInfoArray()
+		usersArray    = getUserInfoArray()
 	)
 
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	i18n.Load()
-
-	db, err := database.ConnectDB("mongodb://root:123456@127.0.0.1:27017/admin", "Movie")
+	db, err := database.ConnectDB("mongodb://root:123456@127.0.0.1:27017/admin", "Server")
 
 	if err != nil {
 		t.Log("连接数据库失败")
@@ -39,23 +34,47 @@ func TestInsertMovie(t *testing.T) {
 	defer db.Close()
 	t.Log("数据库连接成功")
 
-	collection = db.DB.Collection("Movie")
-	//插入一条数据
-	if insertOneRes, err = collection.InsertOne(getContext(), usersArray[0]); err != nil {
-		checkErr(err)
-	}
-
-	fmt.Printf("InsertOne插入的消息ID:%v\n", insertOneRes.InsertedID)
-
+	// collection = db.DB.Collection("Films")
+	collection = db.DB.Collection("Users")
+	
 	//批量插入数据
-	if insertManyRes, err = collection.InsertMany(getContext(), usersArray[1:]); err != nil {
+	if insertManyRes, err = collection.InsertMany(getContext(), usersArray[0:]); err != nil {
 		checkErr(err)
 	}
 	fmt.Printf("InsertMany插入的消息ID:%v\n", insertManyRes.InsertedIDs)
 	var Dinfo = make(map[string]interface{})
-	err = collection.FindOne(getContext(), bson.D{{"Name", "testName_8"}, {"Phone", 188212318}}).Decode(&Dinfo)
+	err = collection.FindOne(getContext(), bson.D{{"Name", "testName_8"}, {"Phone", "188212318"}}).Decode(&Dinfo)
 	fmt.Println(Dinfo)
 	fmt.Println("_id", Dinfo["_id"])
+}
+
+func TestInsertMovies(t *testing.T) {
+
+	var (
+		err           error
+		collection    *mongo.Collection
+		insertManyRes *mongo.InsertManyResult
+		usersArray    = getFilmsInfoArray()
+	)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	db, err := database.ConnectDB("mongodb://root:123456@127.0.0.1:27017/admin", "Server")
+
+	if err != nil {
+		t.Log("连接数据库失败")
+		panic(err)
+	}
+	defer db.Close()
+	t.Log("数据库连接成功")
+
+	collection = db.DB.Collection("Films")
+
+	//批量插入数据
+	if insertManyRes, err = collection.InsertMany(getContext(), usersArray[0:]); err != nil {
+		checkErr(err)
+	}
+	fmt.Printf("InsertMany插入的消息ID:%v\n", insertManyRes.InsertedIDs)
 }
 
 func checkErr(err error) {
@@ -67,7 +86,6 @@ func checkErr(err error) {
 			fmt.Println(err)
 			os.Exit(0)
 		}
-
 	}
 }
 
@@ -76,7 +94,28 @@ func getContext() (ctx context.Context) {
 	return
 }
 
-func GetUserInfoArray() (data []interface{}) {
+func getFilmsInfoArray() (data []interface{}) {
+	var i int64
+	for i = 0; i <= 100; i++ {
+		data = append(data, model.Video{
+			ID:       primitive.NewObjectID(),
+			UserID:     fmt.Sprintf("testName_%d", i+1),
+			UserName:    fmt.Sprintf("1235_%d@gmail.com", i+1),
+			Desc:    fmt.Sprintf("18821231%d", i+1),
+			VideoUrl:  fmt.Sprintf("www.baidu.com"),
+			Tags:  time.Now(),
+			Praises:  rand.Intn(100000),
+			Treads:	 rand.Intn(100000),
+			Comments:	 rand.Intn(100000),
+			Transmits:	 rand.Intn(100000),
+			UpdateTime:	 rand.Intn(100000),
+			Duration: rand.Intn(70),
+		})
+	}
+	return
+}
+
+func getUserInfoArray() (data []interface{}) {
 	var i int64
 	for i = 0; i <= 100; i++ {
 		data = append(data, model.User{
